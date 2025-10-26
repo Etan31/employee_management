ea63a8c39b746be1213caacbb9b2fdbc7768f284
Tristan Ehron Tumbaga <tumbagatristanehron31@gmail.com>
2025-10-26 17:05:39 +0800

    style: added dashboard texts

--- v1/frontend/src/pages/dashboard/Dashboard.jsx
+++ v1/frontend/src/pages/dashboard/Dashboard.jsx
@@ -1,15 +1,31 @@
-import React from 'react'
-import Navigation from '../../components/navigation/Navigation'
+import React from "react";
+import Navigation from "../../components/navigation/Navigation";
 
 function Dashboard() {
   return (
     <>
-    <Navigation/>
-    <main>
-      <h1>Hello</h1>
-    </main>
+      <div className="layout">
+        <Navigation />
+        <main>
+          <header>
+            <h1>Dashboard</h1>
+            {/* Admin & Hr */}
+            <p>
+              Manage employees, departments, and activities, including totals,
+              active staff, recent hires, events, and onboarding, with options
+              to create users and events.
+            </p>
+
+            {/* For the employee */}
+            <p>
+              View total employees, your department info, absences/leave, and
+              recent activities like hires, events, and orientations.
+            </p>
+          </header>
+        </main>
+      </div>
     </>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
