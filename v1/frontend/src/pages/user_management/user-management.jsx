a72ac57ecb5f16a22295c2fceaf123222825e3f4
Tristan Ehron Tumbaga <tumbagatristanehron31@gmail.com>
2025-10-28 08:40:19 +0800

    fix: upddated space of navigation to be clickable.

--- v1/frontend/src/pages/user_management/user-management.jsx
+++ v1/frontend/src/pages/user_management/user-management.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import Navigation from "../../components/navigation/Navigation";
 
 function Usermanagement() {
   return (
-    <div>Usermanagement</div>
+    <>
+      <div className="layout">
+        <Navigation/>
+        <div className="main">
+          <h1>Hellow</h1>
+        </div>
+      </div>
+    </>
   )
 }
 
