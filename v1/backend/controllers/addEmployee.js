416595597ad13559e79ea5cf02ba128fb8c72380
Tristan Ehron Tumbaga <tumbagatristanehron31@gmail.com>
2025-11-02 19:39:53 +0800

    feat: successful adding new employee

---
+++ v1/backend/controllers/addEmployee.js
@@ -0,0 +1,39 @@
+const pool = require("../db/pool");
+const crypto = require("crypto");
+
+exports.addEmployee = async (req, res) => {
+  const data = req.body;
+
+  try {
+    const employeeId = crypto.randomInt(1000000000, 9999999999);
+
+    const date_of_birth = `${data.year}-${data.month}-${data.day}`;
+    const joined_date = `${data.joined_year}-${data.joined_month}-${data.joined_day}`;
+
+    const query = `INSERT INTO employees (emp_id, first_name, middle_name, last_name, date_of_birth, gender, email, phone_number, location, department_id, position_id, joined_date, employment_status) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13) 
+     RETURNING *; `;
+    const values = [
+      employeeId,
+      data.first_name,
+      data.middle_name,
+      data.last_name,
+      date_of_birth,
+      data.gender,
+      data.email,
+      data.phone_number,
+      data.location,
+      data.department_id,
+      data.position_id,
+      joined_date,
+      data.employment_status,
+    ];
+    const result = await pool.query(query, values);
+    res.status(201).json({
+      message: "Employee added successfully",
+      employee: result.rows[0],
+    });
+  } catch (error) {
+    console.error("Error adding employees:", error);
+    res.status(500).json({ error: "Server error" });
+  }
+};
--- v1/backend/routes/auth.js
+++ v1/backend/routes/auth.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const verifyToken = require('../middlewares/verifyToken');
 const { registerUser, loginUser} = require("../controllers/authController");
+const { addEmployee } = require("../controllers/addEmployee");
 
 router.get("/verify", verifyToken, (req, res) => {
   res.json({ success: true, user: req.user });
@@ -9,5 +10,7 @@ router.get("/verify", verifyToken, (req, res) => {
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+
+router.post("/addemployee", addEmployee);
 
 module.exports = router;
