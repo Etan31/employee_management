65c539ac351507bd0f38693f9d2adac31c5ffcdd
Tristan Ehron Tumbaga <tumbagatristanehron31@gmail.com>
2025-11-04 20:44:09 +0800

    feat: query to get the list of users

---
+++ v1/backend/controllers/getEmployeeList.js
@@ -0,0 +1,15 @@
+exports.getEmployeeList = async (req, res) => {
+  try {
+    const result = await poo.query(`
+      SELECT 
+      *
+      FROM users u
+      LEFT JOIN employees e 
+      ON u.user_id = e.user_id;
+    `);
+    res.status(200).json(result.rows);
+  } catch (error) {
+    console.error("Error getting employee list:", error);
+    res.status(500).json({ error: "Failed to fetch manager list" });
+  }
+};
--- v1/backend/routes/auth.js
+++ v1/backend/routes/auth.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const verifyToken = require('../middlewares/verifyToken');
 const { registerUser, loginUser} = require("../controllers/authController");
 const { addEmployee } = require("../controllers/addEmployee");
-
+const { getEmployeeList } = require("../controllers/getEmployeeList");
 router.get("/verify", verifyToken, (req, res) => {
   res.json({ success: true, user: req.user });
 });
@@ -12,5 +12,6 @@ router.post("/register", registerUser);
 router.post("/login", loginUser);
 
 router.post("/addemployee", addEmployee);
+router.post("/getemployeelist", getEmployeeList);
 
 module.exports = router;
