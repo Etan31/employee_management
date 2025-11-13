2a98dd84a5cb97e25921d5283e6d9451802770e3
Tristan Ehron Tumbaga <tumbagatristanehron31@gmail.com>
2025-11-13 20:13:24 +0800

    fix: added threshold for testing

--- v1/backend/jest.config.cjs
+++ v1/backend/jest.config.cjs
@@ -2,5 +2,12 @@ module.exports = {
   testEnvironment: "node",
   roots: ["<rootDir>/__test__"],
   setupFilesAfterEnv: ["<rootDir>/__test__/setup/testSetup.js"],
+  coverageThreshold: {
+    global: {
+      statements: 80,
+      branches: 80,
+      functions: 80,
+      lines: 80,
+    },
+  },
 };
- 
