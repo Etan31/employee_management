c7f040da34ced0caccdbf98df5e0dcfbfaf1711b
Tristan Ehron Tumbaga <tumbagatristanehron31@gmail.com>
2025-11-18 20:32:13 +0800

    fix: changed appending partcipants user id.

--- v1/backend/controllers/addEvent.js
+++ v1/backend/controllers/addEvent.js
@@ -6,26 +6,36 @@ exports.addEvent = async (req, res) => {
 
   try {
     const event_id = crypto.randomInt(1000000000, 9999999999);
-
     const event_date = `${data.year}-${data.month}-${data.day}`;
-     
-    const query = `INSERT INTO events (event_id, title, description, city, municipality, participants, event_date, attachment_url, created_at) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, NOW()::timestamp without time zone)`;
+
+    const attachment_url = req.file ? `/pdf/${req.file.filename}` : null;
+
+    const query = `
+      INSERT INTO events (
+        event_id, title, description, city, municipality, participants, 
+        event_date, attachment_url, created_at
+      ) VALUES (
+        $1, $2, $3, $4, $5, $6, $7, $8, NOW()::timestamp without time zone
+      )
+    `;
+
     const values = [
       event_id,
       data.title,
       data.description,
       data.city,
       data.municipality,
-      data.participants.user_id,
+      data.participantsId,
       event_date,
-      data.attachment_url,
+      attachment_url,
     ];
-    const result = await pool.query(query, values);
 
-    res
-      .status(201)
-      .json({ message: "Event added successfully", event_id: event_id });
-    console.log("Event added successfully", result.rows[0]);
+    await pool.query(query, values);
+
+    res.status(201).json({
+      message: "Event added successfully",
+      event_id,
+    });
   } catch (error) {
     console.error("Error adding event:", error);
     res.status(500).json({ error: "Failed to add event" });
